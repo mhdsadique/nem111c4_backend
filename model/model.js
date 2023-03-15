4292ef84860ff522813e90ac9b585f212a356e82
Muhammed sadique <mhdsadique8@gmail.com>
2023-03-15 19:23:33 +0530

    a123452000

--- model/model.js
+++ model/model.js
@@ -2,13 +2,17 @@
 
 const mongoose=require("mongoose")
 const userSchema=mongoose.Schema({
-    name : {type:String,require:true},
-email : {type:String,require:true},
-gender : {type:String,require:true},
-password : {type:String,require:true},
-age : {type:Number,require:true},
-city : {type:String,require:true},
-user : {type:String,require:true},
+//     name : {type:String,require:true},
+// email : {type:String,require:true},
+// gender : {type:String,require:true},
+// password : {type:String,require:true},
+// age : {type:Number,require:true},
+// city : {type:String,require:true},
+// user : {type:String,require:true},
+firstName:{type:String,require:true},
+lastName:{type:String,require:true},
+password:{type:String,require:true},
+email:{type:String,require:true}
 },{
     versionKey:false
 })
--- model/notemodel.js
+++ model/notemodel.js
@@ -2,10 +2,17 @@
 
 const mongoose=require("mongoose")
 const noteSchema=mongoose.Schema({
-    title  : {type:String,require:true},
-    body : {type:String,require:true},
-    device : {type:String,require:true},
-    no_if_comments : {type:Number,require:true},
+    // title  : {type:String,require:true},
+    // body : {type:String,require:true},
+    // device : {type:String,require:true},
+    // no_if_comments : {type:Number,require:true},
+    image:{type:String,require:true},
+    title:{type:String,require:true},
+    discription:{type:String,require:true},
+    price:{type:Number,require:true},
+    itemfind:{type:String,require:true},
+    findname:{type:String,require:true},
+    user:{type:String,require:true}
 },{
     versionKey:false
 })

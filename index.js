3c341c716059fe41698dd3e64037228e873be3db
Muhammed sadique <mhdsadique8@gmail.com>
2023-03-31 18:00:42 +0530

    cart

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json())
 app.use(cors())
 
 app.use("/users",userRouter)
-app.use(authentication)
+// app.use(authentication)
 app.use("/product",noteRouter)
 app.use(authentication)
 app.use("/cart",cartRouter)
